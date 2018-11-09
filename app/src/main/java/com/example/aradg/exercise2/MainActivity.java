f4fec281f88e5532b3347c3d777ce89202e50286
AradGabay <44049933+AradGabay@users.noreply.github.com>
2018-11-09 12:35:58 +0200

    Final program|Exercise 2

--- app/src/main/java/com/example/aradg/exercise2/MainActivity.java
+++ app/src/main/java/com/example/aradg/exercise2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -14,9 +15,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button cfn,lowerthan,equalto,greaterthan;
-    TextView txt1,txt2,wrongAnswer,rightAnswer;
+    TextView txt1,txt2,rightAnswer,tof;
     Random rnd=new Random();
-    int a,b,wa,ra;
+    int a,b,ra;
 
 
     @Override
@@ -30,55 +31,70 @@ public class MainActivity extends AppCompatActivity {
         equalto=(Button)findViewById(R.id.equalto);
         greaterthan=(Button)findViewById(R.id.greaterthan);
 
+        tof=(TextView)findViewById(R.id.tof);
         txt1=(TextView)findViewById(R.id.txt1);
         txt2=(TextView)findViewById(R.id.txt2);
-        wrongAnswer=(TextView)findViewById(R.id.wronganswer);
         rightAnswer=(TextView)findViewById(R.id.rightanswer);
 
-        a=0;
-        b=0;
-        wa=0;
         ra=0;
 
     }
 
     public void btnclick1(View view) {
         a=rnd.nextInt(100);
-        txt1.setText("Number= "+a);
+        txt1.setText("Number 1= "+a);
         b=rnd.nextInt(100);
-        txt2.setText("Number= "+b);
+        txt2.setText("Number 2= "+b);
+        cfn.setEnabled(false);
 
     }
 
     public void btnclick2(View view) {
-        if(a<b){
-            ra++;
-            rightAnswer.setText("Right answers:"+ra);
-        }else{
-            wa++;
-            wrongAnswer.setText("Wrong answers:"+wa);
-        }
+            if (a < b) {
+                ra++;
+                rightAnswer.setText("Score:" + ra);
+                tof.setText("True");
+                tof.setTextColor(Color.GREEN);
+            } else {
+                tof.setText("False");
+                tof.setTextColor(Color.RED);
+            } a=rnd.nextInt(100);
+        txt1.setText("Number 1= "+a);
+        b=rnd.nextInt(100);
+        txt2.setText("Number 2= "+b);
 
     }
 
     public void btnclick3(View view) {
-        if(a==b){
-            ra++;
-            rightAnswer.setText("Right answers:"+ra);
-        }else{
-            wa++;
-            wrongAnswer.setText("Wrong answers:"+wa);
-        }
+            if (a == b) {
+                ra++;
+                rightAnswer.setText("Score:" + ra);
+                tof.setText("True");
+                tof.setTextColor(Color.GREEN);
+            } else {
+                tof.setText("False");
+                tof.setTextColor(Color.RED);
+            } a=rnd.nextInt(100);
+        txt1.setText("Number 1= "+a);
+        b=rnd.nextInt(100);
+        txt2.setText("Number 2= "+b);
+
     }
 
     public void btnclick4(View view) {
-        if(a>b){
-            ra++;
-            rightAnswer.setText("Right answers:"+ra);
-        }else{
-            wa++;
-            wrongAnswer.setText("Wrong answers:"+wa);
-        }
+            if (a > b) {
+                ra++;
+                rightAnswer.setText("Score:" + ra);
+                tof.setText("True");
+                tof.setTextColor(Color.GREEN);
+            } else {
+                tof.setText("False");
+                tof.setTextColor(Color.RED);
+            } a=rnd.nextInt(100);
+        txt1.setText("Number 1= "+a);
+        b=rnd.nextInt(100);
+        txt2.setText("Number 2= "+b);
 
     }
-}
+    }
+
